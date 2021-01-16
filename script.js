275064536640ed41f0318568f039c7d7b5a2c40a
Quinton <qnistico@aol.com>
2021-01-16 15:01:55 -0500

    good

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $( document ).ready(function() {
     new WOW().init();
   });
+
+  $(document).ready(function() {
+
+    // SideNav Button Initialization
+    $(".button-collapse").sideNav();
+    // SideNav Scrollbar Initialization
+    var sideNavScrollbar = document.querySelector('.custom-scrollbar');
+    var ps = new PerfectScrollbar(sideNavScrollbar);
+  })
